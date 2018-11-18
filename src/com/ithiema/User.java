5dabee36b485a6e6c6eae821621692954715849c
tanlin970511 <710915417@qq.com>
2018-11-18 20:34:43 +0800

    修改后的一个项目

--- src/com/ithiema/User.java
+++ src/com/ithiema/User.java
@@ -4,8 +4,10 @@ public class User {
     public static void main(String[] args) {
        // fall();
     }
+    //这是一个方法
     public  void  fall(){
         System.out.println("你好 ");
+        System.out.println("大家好才是还真的好");
     }
 
 
